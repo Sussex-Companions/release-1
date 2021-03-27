175aa4f3068d0314d566f8f89fd92ecc23bc975b
imBalasankar <bals9@live.com>
2021-03-27 21:36:08 +0530

    Finance Manager navbar js updated

--- SussexCompanions/Staff/FinanceManager/navbar.php
+++ SussexCompanions/Staff/FinanceManager/navbar.php
@@ -56,19 +56,30 @@
 <script>
 /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
 var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
+var dropdownContent1 = dropdown[0].nextElementSibling;
+var dropdownContent2 = dropdown[1].nextElementSibling;
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var dropdownContent = this.nextElementSibling;
-  if (dropdownContent.style.display === "block") {
-  dropdownContent.style.display = "none";
-  } else {
-  dropdownContent.style.display = "block";
-  }
+  dropdown[0].addEventListener("click", function() {
+	if (dropdownContent1.style.display === "block") {
+		dropdownContent1.style.display = "none";
+	} else {
+		if(dropdownContent2.style.display === "block"){
+			dropdownContent2.style.display = "none";  
+		}
+		dropdownContent1.style.display = "block";
+	}
   });
-}
+  
+  dropdown[1].addEventListener("click", function() {
+	if (dropdownContent2.style.display === "block") {
+		dropdownContent2.style.display = "none";
+	} else {
+		if(dropdownContent1.style.display === "block"){
+			dropdownContent1.style.display = "none";  
+		}
+		dropdownContent2.style.display = "block";
+	}
+  });
 </script>
 </body>
 </html> 
